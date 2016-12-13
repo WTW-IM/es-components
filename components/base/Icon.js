fd376d4ec5120653bf5f7d29644b44dceaa6814f
Alex Abenoja <abenojaalex@gmail.com>
2016-12-12 21:40:32 -0700

    Add eslint and lint fixes

--- components/base/Icon.js
+++ components/base/Icon.js
@@ -1,14 +1,23 @@
+/* eslint-disable no-use-before-define */
 import React from 'react';
 import classnames from 'classnames';
 
 Icon.propTypes = {
   /**
+   * CSS classes to apply
+   */
+  className: React.PropTypes.string,
+  /**
    * Name of the icon to display
    */
-  name: React.PropTypes.string.isRequired
+  name: React.PropTypes.string.isRequired,
+  /**
+   * Specify icon size
+   */
+  size: React.PropTypes.oneOf([24, 34, 48])
 };
 
-export default function Icon({className, name, size, ...other}) {
-  var classes = classnames(className, `oe-icon-${name}`, `size-${size}`);
+export default function Icon({ className, name, size, ...other }) {
+  const classes = classnames(className, `oe-icon-${name}`, `size-${size}`);
   return <i className={classes} {...other} />;
 }

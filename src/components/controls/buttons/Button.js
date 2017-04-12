31e8fa1b0f7265efc6dc21eb94000652fd6779ef
Jake Rios <rios.jacob@gmail.com>
2017-04-12 10:05:32 -0600

    Add Button component
    Add Button component that will render the default button or the
    alternative button depending on its props.

---
+++ src/components/controls/buttons/Button.js
@@ -0,0 +1,196 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import tinycolor from 'tinycolor2';
+
+import { buttonSizeStyles } from './button-sizes';
+import { defaultButtonVariants, alternateButtonVariants } from './button-variants';
+
+import { colors } from '../../theme';
+
+import Icon from '../../base/icons/Icon';
+
+function getBlockPropertyValues(isBlock) {
+  if (isBlock) {
+    return {
+      display: 'block',
+      width: '100%'
+    };
+  }
+  return {
+    display: 'initial',
+    width: 'initial'
+  };
+}
+
+const ButtonBase = styled.button`
+  border: none;
+  border-radius: ${props => props.buttonSize.borderRadius};
+  box-sizing: border-box;
+  cursor: pointer;
+  display: ${props => getBlockPropertyValues(props.block).display};
+  font-family: inherit;
+  font-size: ${props => props.buttonSize.fontSize};
+  margin: 0 0 4px 0;
+  padding: ${props => props.buttonSize.padding};
+  text-align: center;
+  width: ${props => getBlockPropertyValues(props.block).width};
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.65;
+  }
+
+  &:active {
+    margin-bottom: 0;
+    margin-top: 4px;
+    box-shadow: 0 0 0 0 transparent;
+  }
+`;
+
+const DefaultButton = styled(ButtonBase)`
+  background-color: ${props => props.buttonVariant.backgroundColor.toRgbString()};
+  border-color: ${props => props.buttonVariant.borderColor};
+  box-shadow: 0 4px 0 0 ${props => props.buttonVariant.boxShadowColor};
+  color: ${props => props.buttonVariant.foregroundColor};
+
+  &[disabled]:hover {
+    background-color: ${props => props.buttonVariant.backgroundColor.toRgbString()};
+  }
+
+  &:hover {
+    background-color: ${props => props.buttonVariant.hoverBackgroundColor};
+  }
+`;
+
+const AlternateButton = styled(ButtonBase)`
+  background-color: ${colors.white};
+  border: 2px solid ${props => props.buttonVariant.borderColor.toRgbString()};
+  border-radius: 0;
+  color: ${props => props.buttonVariant.foregroundColor.toRgbString()};
+
+  &:hover {
+    background-color: ${props => props.buttonVariant.hoverBackgroundColor.toRgbString()};
+    color: ${props => props.buttonVariant.hoverForegroundColor};
+  }
+`;
+
+const AlternateChildrenContainer = styled.div`
+  display: flex;
+  font-weight: bold;
+`;
+
+const AlternateButtonIcon = styled(Icon)`
+  align-self: center;
+  font-weight: bold;
+  margin-left: 5px;
+`;
+
+const LinkButton = styled(ButtonBase)`
+  background-color: transparent;
+  box-shadow: none;
+  color: ${colors.accent};
+  padding: 0;
+  text-decoration: underline;
+
+  &:hover {
+    text-decoration: none;
+  }
+
+  &[disabled]:hover {
+    color: ${colors.accent};
+    text-decoration: underline;
+  }
+
+  &:active {
+    margin: 0 0 4px 0;
+    box-shadow: none;
+  }
+
+  &:hover, &:focus {
+    color: ${tinycolor(colors.primary).darken(15).toRgbString()};
+  }
+
+  &:hover, &:focus, &:active {
+    border-color: transperent;
+  }
+`;
+
+function Button({
+  handleOnClick,
+  children,
+  buttonClasses,
+  type = 'default',
+  size = 'default',
+  block = false,
+  alternative = false,
+    ...buttonProps
+}) {
+  const buttonSize = buttonSizeStyles[size];
+  const isLinkButton = type === 'link';
+  const sharedProps = {
+    block,
+    buttonSize,
+    onClick: handleOnClick,
+    className: buttonClasses,
+    ...buttonProps
+  };
+
+  if (alternative) {
+    const buttonVariant = alternateButtonVariants[type];
+    return (
+      <AlternateButton buttonVariant={buttonVariant} {...sharedProps}>
+        <AlternateChildrenContainer>
+          {children}
+          <AlternateButtonIcon name="arrow-right" lightweight />
+        </AlternateChildrenContainer>
+      </AlternateButton>
+    );
+  } else if (isLinkButton) {
+    return (
+      <LinkButton {...sharedProps}>
+        {children}
+      </LinkButton>
+    );
+  }
+
+  const buttonVariant = defaultButtonVariants[type];
+  return (
+    <DefaultButton buttonVariant={buttonVariant} {...sharedProps}>
+      {children}
+    </DefaultButton>
+  );
+}
+
+const buttonTypes = [
+  'default',
+  'primary',
+  'accent',
+  'success',
+  'info',
+  'warning',
+  'danger'
+];
+
+const buttonSizes = [
+  'lg',
+  'default',
+  'sm',
+  'xs'
+];
+
+Button.propTypes = {
+  /** Function to execute when button is clicked */
+  handleOnClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+  /** Any additional classes to apply to the button */
+  buttonClasses: PropTypes.string,
+  type: PropTypes.oneOf(buttonTypes),
+  size: PropTypes.oneOf(buttonSizes),
+  /** Make the button's width the size of it's parent container */
+  block: PropTypes.bool,
+  /** Render with the alternative styling */
+  alternative: PropTypes.bool
+};
+
+export default Button;
---
+++ src/components/controls/buttons/Button.md
@@ -0,0 +1,164 @@
+## Button types
+
+```
+const buttonExampleStyle = {
+  display: 'flex',
+  flexFlow: 'row wrap',
+  justifyContent: 'space-between',
+  paddingTop: '20px'
+};
+
+function noop() { }
+
+<div style={buttonExampleStyle}>
+  <Button handleOnClick={noop}>Default</Button>
+
+  <Button handleOnClick={noop} type="primary">Primary</Button>
+
+  <Button handleOnClick={noop} type="accent">Accent</Button>
+
+  <Button handleOnClick={noop} type="success">Success</Button>
+
+  <Button handleOnClick={noop} type="info">Information</Button>
+
+  <Button handleOnClick={noop} type="warning">Warning</Button>
+
+  <Button handleOnClick={noop} type="danger">Danger</Button>
+
+  <Button handleOnClick={noop} type="link">Link</Button>
+</div>
+```
+
+### Alternative button types
+
+Setting the ``alternative`` prop will give the button the alternative style for each type.
+
+```
+const buttonExampleStyle = {
+  display: 'flex',
+  flexFlow: 'row wrap',
+  justifyContent: 'space-between',
+  paddingTop: '20px'
+};
+
+function noop() { }
+
+<div style={buttonExampleStyle}>
+  <Button handleOnClick={noop} alternative>Default</Button>
+
+  <Button handleOnClick={noop} type="primary" alternative>Primary</Button>
+
+  <Button handleOnClick={noop} type="accent" alternative>Accent</Button>
+
+  <Button handleOnClick={noop} type="success" alternative>Success</Button>
+
+  <Button handleOnClick={noop} type="info" alternative>Information</Button>
+
+  <Button handleOnClick={noop} type="warning" alternative>Warning</Button>
+
+  <Button handleOnClick={noop} type="danger" alternative>Danger</Button>
+</div>
+```
+
+## Button sizes
+
+Including a size will set buttons to a specific size
+
+```
+const buttonExampleStyle = {
+  marginBottom: '5px'
+};
+
+function noop() {}
+
+<div>
+  <div style={buttonExampleStyle}>
+    <Button
+      handleOnClick={noop}
+      size="lg"
+    >
+      Large
+    </Button>
+  </div>
+
+  <div style={buttonExampleStyle}>
+    <Button
+      handleOnClick={noop}
+      size="default"
+    >
+      Default
+    </Button>
+  </div>
+
+  <div style={buttonExampleStyle}>
+    <Button
+      handleOnClick={noop}
+      size="sm"
+    >
+      Small
+    </Button>
+  </div>
+
+  <div style={buttonExampleStyle}>
+    <Button
+      handleOnClick={noop}
+      size="xs"
+    >
+      Extra small
+    </Button>
+  </div>
+
+</div>
+```
+
+Setting the ``block`` property will force the button to expand to the width of it's parent container.
+
+```
+const buttonContainerStyle = {
+  width: '300px',
+  height: '50px',
+  padding: '10px',
+  ['background-color']: '#ddd',
+  border: '1px solid #444'
+};
+
+function noop() { }
+
+<div style={buttonContainerStyle}>
+  <Button onClick={noop} block type="primary">Block button</Button>
+</div>
+
+```
+
+Any additional prop sent will be included on the button. For example, setting the ``disabled`` property will put the button into a disabled state, making it unclickable.
+
+```
+const buttonExampleStyle = {
+  display: 'flex',
+  flexFlow: 'row wrap',
+  justifyContent: 'space-between',
+  paddingTop: '20px'
+};
+
+function noop() { }
+
+<div style={buttonExampleStyle}>
+  <Button handleOnClick={noop} disabled>Default</Button>
+
+  <Button handleOnClick={noop} type="primary" disabled>Primary</Button>
+
+  <Button handleOnClick={noop} type="accent" disabled>Accent</Button>
+
+  <Button handleOnClick={noop} type="success" disabled>Success</Button>
+
+  <Button handleOnClick={noop} type="info" disabled>Information</Button>
+
+  <Button handleOnClick={noop} type="warning" disabled>Warning</Button>
+
+  <Button handleOnClick={noop} type="danger" disabled>Danger</Button>
+
+  <Button handleOnClick={noop} type="link" disabled>Link</Button>
+</div>
+
+
+```

d2857d9d4f9890b0003f9a2abe8cf47b8802a1ba
kenada <kenneth.adams5@extendhealth.com>
2017-04-12 14:29:15 -0600

    Converted Drawer to use styled-components, updated enzyme version

--- src/components/base/containers/Drawer.specs.js
+++ src/components/base/containers/Drawer.specs.js
@@ -7,54 +7,33 @@ import { mount } from 'enzyme';
 import Drawer from './Drawer';
 import DrawerPanel from './DrawerPanel';
 
-jest.mock('../../../styles/variables/colors.less', () => ({}));
-jest.mock('./drawer.less', () => ({}));
-
 describe('drawer component', () => {
   describe('drawer', () => {
     let drawerInstance;
 
     beforeEach(() => {
       drawerInstance = mount(<Drawer className="important">
-        <DrawerPanel header="collapse 1" key="1">first</DrawerPanel>
-        <DrawerPanel header="collapse 2" noPadding key="2">second</DrawerPanel>
-        <DrawerPanel header="collapse 3" key="3" className="alt-drawer">third</DrawerPanel>
+        <DrawerPanel header="collapse 1" key="1" className="first">first</DrawerPanel>
+        <DrawerPanel header="collapse 2" key="2" className="second" noPadding>second</DrawerPanel>
+        <DrawerPanel header="collapse 3" key="3" className="third">third</DrawerPanel>
       </Drawer>);
     });
 
     it('renders as expected', () => {
       const tree = renderer.create(
         <Drawer className="important">
-          <DrawerPanel header="collapse 1" key="1">first</DrawerPanel>
-          <DrawerPanel header="collapse 2" noPadding key="2">second</DrawerPanel>
-          <DrawerPanel header="collapse 3" key="3" className="alt-drawer">third</DrawerPanel>
+          <DrawerPanel header="collapse 1" key="1" className="first">first</DrawerPanel>
+          <DrawerPanel header="collapse 2" key="2" className="second" noPadding>second</DrawerPanel>
+          <DrawerPanel header="collapse 3" key="3" className="third">third</DrawerPanel>
         </Drawer>
       ).toJSON();
 
       expect(tree).toMatchSnapshot();
     });
 
-    it('adds additional className to the root element', () => {
-      const drawerDiv = drawerInstance.find('.drawer');
-      expect(drawerDiv.hasClass('important')).toBe(true);
-    });
-
-    it('creates the panels closed by default', () => {
-      const panels = drawerInstance
-                      .find('[aria-expanded]')
-                      .filterWhere(node => !node.prop('aria-expanded'));
-
-      expect(panels.length).toBe(3);
-    });
-
-    it('adds className to a panel element', () => {
-      const drawerPanel = drawerInstance.find('.drawer-panel').last();
-      expect(drawerPanel.hasClass('alt-drawer')).toBe(true);
-    });
-
     it('click should toggle panel state', () => {
-      const firstPanel = drawerInstance.find('.drawer-panel').first();
-      const panelHeader = firstPanel.find('.drawer-panel__header');
+      const firstPanel = drawerInstance.find('.first');
+      const panelHeader = firstPanel.find({ role: 'tab' });
 
       expect(panelHeader.prop('aria-expanded')).toBe(false);
 
@@ -66,19 +45,21 @@ describe('drawer component', () => {
     });
 
     it('should allow more than one drawer to be opened at a time', () => {
-      const firstPanel = drawerInstance.find('.drawer-panel__header').first();
-      const lastPanel = drawerInstance.find('.drawer-panel__header').last();
+      const firstPanel = drawerInstance.find('.first');
+      const firstHeader = firstPanel.find({ role: 'tab' });
+      const thirdPanel = drawerInstance.find('.third');
+      const thirdHeader = thirdPanel.find({ role: 'tab' });
 
-      expect(firstPanel.prop('aria-expanded')).toBe(false);
-      expect(lastPanel.prop('aria-expanded')).toBe(false);
+      expect(firstHeader.prop('aria-expanded')).toBe(false);
+      expect(thirdHeader.prop('aria-expanded')).toBe(false);
 
-      firstPanel.find('.drawer-panel__header').simulate('click');
-      expect(firstPanel.prop('aria-expanded')).toBe(true);
-      expect(lastPanel.prop('aria-expanded')).toBe(false);
+      firstHeader.simulate('click');
+      expect(firstHeader.prop('aria-expanded')).toBe(true);
+      expect(thirdHeader.prop('aria-expanded')).toBe(false);
 
-      lastPanel.find('.drawer-panel__header').simulate('click');
-      expect(firstPanel.prop('aria-expanded')).toBe(true);
-      expect(lastPanel.prop('aria-expanded')).toBe(true);
+      thirdHeader.simulate('click');
+      expect(firstHeader.prop('aria-expanded')).toBe(true);
+      expect(thirdHeader.prop('aria-expanded')).toBe(true);
     });
 
     it('should set multiple default panels open at start', () => {
@@ -93,32 +74,25 @@ describe('drawer component', () => {
       expect(panels.at(1).prop('aria-expanded')).toBe(true);
       expect(panels.at(2).prop('aria-expanded')).toBe(true);
     });
-
-    it('should remove padding class on panel with noPadding attrib', () => {
-      const panels = drawerInstance.find('.drawer-panel__body');
-      expect(panels.at(0).find('.drawer-panel__body--padded').length).toBe(1);
-      expect(panels.at(1).find('.drawer-panel__body--padded').length).toBe(0);
-      expect(panels.at(2).find('.drawer-panel__body--padded').length).toBe(1);
-    });
   });
 
   describe('accordion', () => {
     let accordionInstance;
 
     beforeEach(() => {
-      accordionInstance = mount(<Drawer className="important" isAccordion defaultActiveKeys="1">
-        <DrawerPanel header="collapse 1" key="1">first</DrawerPanel>
-        <DrawerPanel header="collapse 2" key="2">second</DrawerPanel>
-        <DrawerPanel header="collapse 3" key="3" className="important">third</DrawerPanel>
+      accordionInstance = mount(<Drawer isAccordion defaultActiveKeys="1">
+        <DrawerPanel header="collapse 1" key="1" className="first">first</DrawerPanel>
+        <DrawerPanel header="collapse 2" key="2" className="second">second</DrawerPanel>
+        <DrawerPanel header="collapse 3" key="3" className="third">third</DrawerPanel>
       </Drawer>);
     });
 
     it('renders as expected', () => {
       const tree = renderer.create(
-        <Drawer className="important" isAccordion defaultActiveKeys="1">
-          <DrawerPanel header="collapse 1" key="1">first</DrawerPanel>
-          <DrawerPanel header="collapse 2" key="2">second</DrawerPanel>
-          <DrawerPanel header="collapse 3" key="3" className="important">third</DrawerPanel>
+        <Drawer isAccordion defaultActiveKeys="1">
+          <DrawerPanel header="collapse 1" key="1" className="first">first</DrawerPanel>
+          <DrawerPanel header="collapse 2" key="2" className="second">second</DrawerPanel>
+          <DrawerPanel header="collapse 3" key="3" className="third">third</DrawerPanel>
         </Drawer>
       ).toJSON();
 
@@ -134,15 +108,17 @@ describe('drawer component', () => {
     });
 
     it('should only allow one drawer to be opened at a time', () => {
-      const firstPanel = accordionInstance.find('.drawer-panel__header').first();
-      const lastPanel = accordionInstance.find('.drawer-panel__header').last();
+      const firstPanel = accordionInstance.find('.first');
+      const firstHeader = firstPanel.find({ role: 'tab' });
+      const thirdPanel = accordionInstance.find('.third');
+      const thirdHeader = thirdPanel.find({ role: 'tab' });
 
-      expect(firstPanel.prop('aria-expanded')).toBe(true);
-      expect(lastPanel.prop('aria-expanded')).toBe(false);
+      expect(firstHeader.prop('aria-expanded')).toBe(true);
+      expect(thirdHeader.prop('aria-expanded')).toBe(false);
 
-      lastPanel.find('.drawer-panel__header').simulate('click');
-      expect(firstPanel.prop('aria-expanded')).toBe(false);
-      expect(lastPanel.prop('aria-expanded')).toBe(true);
+      thirdHeader.simulate('click');
+      expect(firstHeader.prop('aria-expanded')).toBe(false);
+      expect(thirdHeader.prop('aria-expanded')).toBe(true);
     });
   });
 });

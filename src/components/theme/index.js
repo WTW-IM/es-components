c871181c727d0d33b308565cf5ddaa97aa9af558
Jake Rios <rios.jacob@gmail.com>
2017-04-12 09:55:02 -0600

    Add theme for styled-components
    Fix webpack configuration.
    Update lint to run on src directory
    Update babel compilation to run against src directory

---
+++ src/components/theme/index.js
@@ -0,0 +1,17 @@
+export const colors = {
+  white: '#fff',
+  red: '#af140c',
+  black: '#000',
+  grayDarkest: '#444',
+  grayDark: '#888',
+  gray: '#aaa',
+  grayLight: '#ddd',
+  grayLighter: '#eee',
+  success: '#060',
+  accent: '#5a0c6f',
+  information: '#1b6284',
+  warning: '#ebaf00',
+  danger: '#af140c',
+  advisor: '#ff6310',
+  errorText: '#df190f'
+};

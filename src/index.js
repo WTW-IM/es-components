21d83c4fc9c02f67f85bc514c6e300cf2b20a2d9
Jake Rios <rios.jacob@gmail.com>
2017-04-14 13:40:14 -0600

    Add exports
    Add exports for:
    - Icon
    - Drawer
    - DrawerPanel
    - FormGroup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+export Icon from './components/base/icons/Icon';
+
+export Alert from './components/base/containers/Alert';
+export Drawer from './components/base/containers/Drawer';
+export DrawerPanel from './components/base/containers/DrawerPanel';
+export Fieldset from './components/base/containers/Fieldset';
+export FormGroup from './components/base/containers/FormGroup';
+
 export Button from './components/controls/buttons/Button';
+export RadioGroup from './components/controls/radio-buttons/RadioGroup';

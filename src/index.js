70091281a63c70e3084de951a15ba7e33a21adff
kenada <kenneth.adams5@extendhealth.com>
2017-04-18 13:45:26 -0600

    some cleanup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export Drawer from './components/base/containers/Drawer';
 export DrawerPanel from './components/base/containers/DrawerPanel';
 export Fieldset from './components/base/containers/Fieldset';
 export FormGroup from './components/base/containers/FormGroup';
+export Tooltip from './components/base/containers/Tooltip';
 
 export Button from './components/controls/buttons/Button';
 export RadioGroup from './components/controls/radio-buttons/RadioGroup';

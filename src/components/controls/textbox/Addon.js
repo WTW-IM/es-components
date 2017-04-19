ae406510e64b77d5a3d84acc3e94838e4f583155
kenada <kenneth.adams5@extendhealth.com>
2017-04-19 15:28:54 -0600

    slipping in a small fix to textbox styles

--- src/components/controls/textbox/Addon.js
+++ src/components/controls/textbox/Addon.js
@@ -9,14 +9,17 @@ const Addon = styled.span`
   border: 1px solid ${props => props.borderColor};
   font-size: ${sizes.baseFontSize}px;
   font-weight: normal;
+  line-height: 1.4;
   padding: 6px 12px;
 
   &.prepend {
     border-right: 0;
+    border-radius: 2px 0 0 2px;
   }
 
   &.append {
     border-left: 0;
+    border-radius: 0 2px 2px 0;
     margin-right: ${props => (props.inline ? '20px' : 'initial')};
   }
 `;

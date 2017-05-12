c8f23f9cf5056caadc45415f0dfd878e7c69a953
kenada <kenneth.adams5@extendhealth.com>
2017-05-12 09:26:33 -0600

    added aria-control to drawer panel with unique id, updated drawer docs, updated react-smooth-collapse to support additional props

---
+++ src/components/util/genId.specs.js
@@ -0,0 +1,22 @@
+import genId from './genId';
+
+describe('genId', () => {
+  it('generates a 6 character string', () => {
+    const result = genId();
+
+    expect(result.length).toBe(6);
+  });
+
+  it('generates only alpha characters', () => {
+    const result = genId();
+
+    expect(/^[a-zA-Z]+$/.test(result)).toBeTrue;
+  });
+
+  it('generates a unique string each subsequent execution', () => {
+    const result1 = genId();
+    const result2 = genId();
+
+    expect(result1).not.toEqual(result2);
+  });
+});

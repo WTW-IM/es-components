bd261f98276485944f3d3ce4d4895488b0d8449b
kenada <kenneth.adams5@extendhealth.com>
2017-10-04 13:54:54 -0600

    removed popover help icon

--- src/components/controls/datepicker/DatePicker.js
+++ src/components/controls/datepicker/DatePicker.js
@@ -4,25 +4,10 @@ import PropTypes from 'prop-types';
 import { noop } from 'lodash';
 import Icon from '../../base/icons/Icon';
 import Textbox from '../textbox/Textbox';
-import PopoverLink from '../../containers/popover/PopoverLink';
 import ReactDatePicker from 'react-datepicker';
 import moment from 'moment';
 import './datepicker.less';
-import styled from 'styled-components';
 import uncontrollable from 'uncontrollable';
-
-const Container = styled.div`display: flex;`;
-
-const Help = styled.div`
-  margin-left: 0.5em;
-  position: relative;
-  top: 26px;
-`;
-
-const List = styled.ul`
-  margin: 10px 0;
-  padding: 0 20px;
-`;
 
 class DateTextbox extends React.PureComponent {
   focus() {
@@ -49,62 +34,17 @@ export const DatePicker = props => {
       prependContent={prependedIcon}
     />
   );
-  const helpText = (
-    <small>
-      <List>
-        <li>
-          <strong>Left</strong>: Move to the previous day
-        </li>
-        <li>
-          <strong>Right</strong>: Move to the next day
-        </li>
-        <li>
-          <strong>Up</strong>: Move to the previous week
-        </li>
-        <li>
-          <strong>Down</strong>: Move to the next week
-        </li>
-        <li>
-          <strong>PgUp</strong>: Move to the previous month
-        </li>
-        <li>
-          <strong>PgDn</strong>: Move to the next month
-        </li>
-        <li>
-          <strong>Home</strong>: Move to the previous year
-        </li>
-        <li>
-          <strong>End</strong>: Move to the next year
-        </li>
-        <li>
-          <strong>Enter/Esc/Tab</strong>: Close the calendar
-        </li>
-      </List>
-    </small>
-  );
 
   const handleChange = date => {
     onChange(date);
   };
 
   return (
-    <Container>
-      <ReactDatePicker
-        selected={selectedDate}
-        onChange={handleChange}
-        customInput={textbox}
-      />
-
-      <Help>
-        <PopoverLink
-          popoverTitle="Datepicker Keyboard Shortcuts"
-          popoverContent={helpText}
-          suppressUnderline
-        >
-          <Icon name="question-sign" />
-        </PopoverLink>
-      </Help>
-    </Container>
+    <ReactDatePicker
+      selected={selectedDate}
+      onChange={handleChange}
+      customInput={textbox}
+    />
   );
 };
 

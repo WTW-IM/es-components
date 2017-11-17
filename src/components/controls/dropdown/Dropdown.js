0448ca6e43e4bbb002637c9705472603bf009afb
Jake Rios <jacobrios@fastmail.com>
2017-11-17 10:39:48 -0700

    Update dropdown to use onChange and onBlur
    Use onChange and onBlur props instead of the previous functions in order to be more consistent with event handling for inputs.
    
    Convert Dropdown to a stateless functional component.

--- src/components/controls/dropdown/Dropdown.js
+++ src/components/controls/dropdown/Dropdown.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { noop, isNil } from 'lodash';
+import { noop } from 'lodash';
 
 import Label from '../Label';
 import { LabelText, SelectBase } from '../BaseControls';
@@ -15,128 +15,79 @@ const optionsShape = {
   optionValue: PropTypes.string.isRequired
 };
 
-export default class Dropdown extends React.Component {
-  static propTypes = {
-    labelText: PropTypes.string.isRequired,
-    /** The name of the select element*/
-    name: PropTypes.string,
-    /** Display label inline with dropdown */
-    inline: PropTypes.bool,
-    options: PropTypes.arrayOf(PropTypes.shape(optionsShape)),
-    /** Display a default first option */
-    includeDefaultFirstOption: PropTypes.bool,
-    /** Prevent default first option from being selected */
-    isDefaultFirstOptionDisabled: PropTypes.bool,
-    /** The text of the first option displayed */
-    firstOptionDisplayText: PropTypes.string,
-    /** The currently selected value */
-    selectedValue: PropTypes.string,
-    /** Display label and text with contextual state colorings */
-    validationState: PropTypes.oneOf([
-      'normal',
-      'success',
-      'warning',
-      'danger'
-    ]),
-    /** Function to execute when the dropdown value changes */
-    onOptionChanged: PropTypes.func,
-    /** Function to execute when the dropdown loses focus */
-    onDropdownFocusLost: PropTypes.func
-  };
+function Dropdown({
+  labelText,
+  name,
+  options,
+  inline = false,
+  includeDefaultFirstOption = true,
+  isDefaultFirstOptionDisabled = true,
+  firstOptionDisplayText = '--',
+  value = '',
+  onChange = noop,
+  onBlur = noop,
+  validationState = 'normal',
+  ...rest
+}) {
+  const firstOption = includeDefaultFirstOption
+    ? <option disabled={isDefaultFirstOptionDisabled} value="">
+        {firstOptionDisplayText}
+      </option>
+    : null;
 
-  static defaultProps = {
-    inline: false,
-    includeDefaultFirstOption: true,
-    isDefaultFirstOptionDisabled: true,
-    firstOptionDisplayText: '--',
-    selectedValue: '',
-    onOptionChanged: noop,
-    onDropdownFocusLost: noop,
-    validationState: 'normal'
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentValue: isNil(props.selectedValue) ? '' : props.selectedValue
-    };
-
-    this.handleOptionChanged = this.handleOptionChanged.bind(this);
-    this.handleFocusLost = this.handleFocusLost.bind(this);
-  }
-
-  componentWillReceiveProps({ selectedValue }) {
-    this.setState(() => ({
-      currentValue: isNil(selectedValue) ? '' : selectedValue
-    }));
-  }
-
-  handleEvent(event, handlerName) {
-    const { value } = event.target;
-
-    const handler = this.props[handlerName];
-    this.setState({ currentValue: value }, () => handler(value));
-  }
-
-  handleOptionChanged(event) {
-    this.handleEvent(event, 'onOptionChanged');
-  }
-
-  handleFocusLost(event) {
-    this.handleEvent(event, 'onDropdownFocusLost');
-  }
-
-  render() {
-    const {
-      labelText,
-      name,
-      inline,
-      options,
-      includeDefaultFirstOption,
-      isDefaultFirstOptionDisabled,
-      firstOptionDisplayText,
-      validationState,
-      ...selectProps
-    } = this.props;
-
-    const { currentValue } = this.state;
-
-    const firstOption = includeDefaultFirstOption
-      ? <option disabled={isDefaultFirstOptionDisabled} value="">
-          {firstOptionDisplayText}
-        </option>
-      : null;
-
-    const selectOptions = options.map(opt => {
-      const optionKey = opt.optionValue.replace(/\s/g, '');
-      return (
-        <option key={optionKey} value={opt.optionValue}>
-          {opt.optionText}
-        </option>
-      );
-    });
-
+  const selectOptions = options.map(opt => {
+    const optionKey = opt.optionValue.replace(/\s/g, '');
     return (
-      <Label inline={inline}>
-        <LabelText
-          foregroundColor={validationTextColor[validationState]}
-          inline={inline}
-        >
-          {labelText}
-        </LabelText>
-        <SelectBase
-          name={name}
-          value={currentValue}
-          onChange={this.handleOptionChanged}
-          onBlur={this.handleFocusLost}
-          {...validationInputColor[validationState]}
-          {...selectProps}
-        >
-          {firstOption}
-          {selectOptions}
-        </SelectBase>
-      </Label>
+      <option key={optionKey} value={opt.optionValue}>
+        {opt.optionText}
+      </option>
     );
-  }
+  });
+
+  return (
+    <Label inline={inline}>
+      <LabelText
+        foregroundColor={validationTextColor[validationState]}
+        inline={inline}
+      >
+        {labelText}
+      </LabelText>
+      <SelectBase
+        name={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        {...validationInputColor[validationState]}
+        {...rest}
+      >
+        {firstOption}
+        {selectOptions}
+      </SelectBase>
+    </Label>
+  );
 }
+
+Dropdown.propTypes = {
+  labelText: PropTypes.string.isRequired,
+  /** The name of the select element*/
+  name: PropTypes.string,
+  /** Display label inline with dropdown */
+  inline: PropTypes.bool,
+  options: PropTypes.arrayOf(PropTypes.shape(optionsShape)),
+  /** Display a default first option */
+  includeDefaultFirstOption: PropTypes.bool,
+  /** Prevent default first option from being selected */
+  isDefaultFirstOptionDisabled: PropTypes.bool,
+  /** The text of the first option displayed */
+  firstOptionDisplayText: PropTypes.string,
+  /** The currently selected value */
+  value: PropTypes.string,
+  /** Display label and text with contextual state colorings */
+  validationState: PropTypes.oneOf(['normal', 'success', 'warning', 'danger']),
+  /** Function to execute when the dropdown value changes */
+  onChange: PropTypes.func,
+  /** Function to execute when the dropdown loses focus */
+  onBlur: PropTypes.func
+};
+
+export default Dropdown;
--- src/components/controls/dropdown/Dropdown.md
+++ src/components/controls/dropdown/Dropdown.md
@@ -1,29 +1,4 @@
-```
-const options = [{
-  optionText: 'Eastern',
-  optionValue: 'east'
-}, {
-  optionText: 'Central',
-  optionValue: 'central'
-}, {
-  optionText: 'Western',
-  optionValue: 'west'
-}];
-
-<div>
-  <Dropdown
-    labelText="Region"
-    options={options}
-  />
-  <Dropdown
-    labelText="inline"
-    options={options}
-    inline
-  />
-</div>
-```
-
-Passing an `onOptionChanged` function will execute with the value of the dropdown when the option has changed.
+Passing an `onChange` function will execute with the value of the dropdown when the option has changed.
 
 ```
 const options = [{
@@ -37,18 +12,47 @@ const options = [{
   optionValue: 'linux'
 }];
 
-function onOptionChanged(value) {
-  alert(`You selected ${value}`);
+class DropdownChangedExample extends React.Component {
+  constructor() {
+    this.state = { selectedValue: '' };
+
+    this.optionChanged = this.optionChanged.bind(this)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.selectedValue !== this.state.selectedValue && this.state.selectedValue !== '') {
+      alert(`You selected ${this.state.selectedValue}`);
+    }
+  }
+
+  optionChanged(event) {
+    this.setState({ selectedValue: event.target.value });
+  }
+
+  render() {
+    return (
+      <Dropdown
+        inline={this.props.inline}
+        value={this.state.selectedValue}
+        labelText="Operating Systems"
+        isDefaultFirstOptionDisabled={false}
+        options={options}
+        onChange={this.optionChanged}
+      />
+    );
+  }
 }
 
-<Dropdown
-  labelText="Choose your operating system"
-  options={options}
-  onOptionChanged={onOptionChanged}
-/>
+
+<div>
+  <DropdownChangedExample inline={false} />
+  <p>Dropdowns can also be rendered inline</p>
+  <DropdownChangedExample inline />
+</div>
+
 ```
 
-Passing an `onDropdownFocusLost` function will execute with the value of the dropdown when the dropdown loses focus.
+Passing an `onBlur` function will execute with the value of the dropdown when the dropdown loses focus.
 
 ```
 const options = [{
@@ -68,15 +72,37 @@ const options = [{
   optionValue: 'boat'
 }];
 
-function onDropdownFocusLost(value) {
-  alert(`You selected ${value}`);
+class DropdownBlurExample extends React.Component {
+  constructor() {
+    this.state = { selectedValue: '' };
+
+    this.optionChanged = this.optionChanged.bind(this);
+    this.displayChosenValue = this.displayChosenValue.bind(this);
+  }
+
+  optionChanged(event) {
+    this.setState({ selectedValue: event.target.value });
+  }
+
+  displayChosenValue(event) {;
+    alert(`You selected ${event.target.value}`);
+  }
+
+  render() {
+    return (
+      <Dropdown
+        value={this.state.selectedValue}
+        labelText="Choose your favorite vehicle"
+        isDefaultFirstOptionDisabled={false}
+        options={options}
+        onChange={this.optionChanged}
+        onBlur={this.displayChosenValue}
+      />
+    );
+  }
 }
 
-<Dropdown
-  labelText="Choose your favorite vehicle"
-  options={options}
-  onDropdownFocusLost={onDropdownFocusLost}
-/>
+<DropdownBlurExample />
 ```
 
 ### Validation states
@@ -93,25 +119,34 @@ const options = [{
   optionValue: 'danger'
 }];
 
-<div>
-  <Dropdown
-    labelText="Success"
-    options={options}
-    validationState="success"
-  />
+class DropdownExample extends React.Component {
+  constructor() {
+    this.state = { selectedOption: options.find(opt => opt.optionValue === 'success') };
 
-  <Dropdown
-    labelText="Warning"
-    options={options}
-    validationState="warning"
-  />
+    this.optionChanged = this.optionChanged.bind(this);
+  }
 
-  <Dropdown
-    labelText="danger"
-    options={options}
-    validationState="danger"
-  />
-</div>
+  optionChanged(event) {
+    const value = event.target.value;
+    const selectedOption = options.find(opt => opt.optionValue === value);
+    this.setState({ selectedOption });
+  }
+
+  render() {
+    const { selectedOption } = this.state;
+    return (
+      <Dropdown
+        labelText={selectedOption.optionText}
+        options={options}
+        validationState={selectedOption.optionValue}
+        onChange={this.optionChanged}
+        value={selectedOption.optionValue}
+      />
+    );
+  }
+}
+
+<DropdownExample />
 ```
 
 Any additional props will get passed to the select HTML element.
@@ -131,10 +166,34 @@ const options = [{
   optionValue: 'iv'
 }];
 
-<Dropdown
-  labelText="Suffix"
-  options={options}
-  selectedValue="sr"
-  disabled
-/>
+class DropdownExample extends React.Component {
+  constructor() {
+    this.state = { selectedValue: '' };
+
+    this.optionChanged = this.optionChanged.bind(this)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.selectedValue !== this.state.selectedValue && this.state.selectedValue !== '') {
+      alert(`You selected ${this.state.selectedValue}`);
+    }
+  }
+
+  optionChanged(event) {
+    this.setState({ selectedValue: event.target.value });
+  }
+
+  render() {
+    return (
+      <Dropdown
+        labelText="Suffix"
+        options={options}
+        value="sr"
+        disabled
+      />
+    );
+  }
+}
+
+<DropdownExample />
 ````

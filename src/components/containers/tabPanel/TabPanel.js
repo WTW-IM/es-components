b3e02e97d6274925eed33fd70768930eb69e12d9
Nayshlok <borlandavid@gmail.com>
2017-12-12 09:04:31 -0700

    Refactor tab panels, some styling
    Refactored the logic to be a bit more concise and cleaner.

--- src/components/containers/tabPanel/TabPanel.js
+++ src/components/containers/tabPanel/TabPanel.js
@@ -5,43 +5,125 @@ import styled from 'styled-components';
 
 const SelectedButton = styled.button`
   background-color: ${colors.white};
+  border: 1px solid ${colors.gray};
+  border-bottom: 1px transparent;
+  border-radius: 2px 2px 0 0;
   color: ${colors.black};
   display: inline-block;
-  border-top: 1px solid ${colors.gray};
-  border-left: 1px solid ${colors.gray};
-  border-right: 1px solid ${colors.gray};
+  font-size: 18px;
+  font-family: Arial, sans-serif;
+  margin-right: 2px;
+  margin-bottom: -1px;
+  padding: 10px 15px;
 `;
 
 const TabButton = styled.button`
   background-color: ${colors.white};
+  border: 0px;
+  border-bottom: 1px;
   color: ${colors.linkColor};
   display: inline-block;
+  font-size: 18px;
+  font-family: Arial, sans-serif;
+  padding: 10px 15px;
+  &:hover: {
+    background-color: ${colors.grayDarker};
+  }
+`;
+
+const SelectedDropdown = styled.select`
+  background-color: ${colors.white};
+  border: 1px solid ${colors.gray};
+  border-bottom: 1px transparent;
+  border-radius: 2px 2px 0 0;
+  box-shadow: 0px;
+  color: ${colors.black};
+  display: inline-block;
+  font-family: Arial, sans-serif;
+  font-size: 18px;
+  margin-right: 2px;
+  padding: 10px 15px;
+`;
+
+const OtherDropdown = styled.select`
+  background-color: ${colors.white};
+  border: 0px;
+  border-bottom: 1px;
+  color: ${colors.linkColor};
+  display: inline-block;
+  font-size: 18px;
+  font-family: Arial, sans-serif;
+  padding: 10px 15px;
   &:hover: {
     background-color: ${colors.grayDarker};
   }
 `;
 
 const ContentDiv = styled.div`
-  background-color: ${colors.grayLightest};
+  border-top: 1px solid ${colors.grayLight};
 `;
 
-function Tab({ name, selected, action, value, ...props }) {
-  return (
-    <span>
-      {selected ? (
-        <SelectedButton>{name}</SelectedButton>
-      ) : (
-        <TabButton onClick={() => action(value)}>{name}</TabButton>
-      )}
-    </span>
+function Tab({ name, selected, action, ...props }) {
+  return selected ? (
+    <SelectedButton>{name}</SelectedButton>
+  ) : (
+    <TabButton onClick={() => action(name)}>{name}</TabButton>
   );
 }
 
 Tab.propTypes = {
   name: PropTypes.string.isRequired,
   selected: PropTypes.bool.isRequired,
-  action: PropTypes.func,
-  value: PropTypes.string.isRequired
+  action: PropTypes.func
+};
+
+function TabList({ options, name, selected, action, ...props }) {
+  const update = event => {
+    const value = event.target.value;
+    action(value);
+  };
+
+  const Title = <option disabled>{name}</option>;
+
+  const selectOptions = options.map(opt => {
+    const optionKey = opt.optionText.replace(/\s/g, '');
+    return (
+      <option key={optionKey} value={optionKey}>
+        {opt.optionText}
+      </option>
+    );
+  });
+
+  return selected ? (
+    <SelectedDropdown value={name} onChange={update}>
+      {Title}
+      {selectOptions}
+    </SelectedDropdown>
+  ) : (
+    <OtherDropdown value={name} onChange={update}>
+      {Title}
+      {selectOptions}
+    </OtherDropdown>
+  );
+}
+
+TabList.propTypes = {
+  options: PropTypes.array.isRequired,
+  name: PropTypes.string.isRequired,
+  selected: PropTypes.bool.isRequired,
+  action: PropTypes.func
+};
+
+function TabItem({ name, children }) {
+  return <span />;
+}
+
+TabItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  children: PropTypes.oneOf([
+    PropTypes.element,
+    PropTypes.arrayOf(PropTypes.element)
+  ])
 };
 
 function TabContent({ children, ...props }) {
@@ -66,25 +148,42 @@ class TabPanel extends React.Component {
     this.setState({ value: name });
   };
 
+  isChildSelected(value, index) {
+    return value === this.state.value || (!this.state.value && index === 0);
+  }
+
   render() {
-    let contentContainer;
+    let content = null;
     const elements = React.Children.map(this.props.children, (child, i) => {
-      let content = null;
+      let selected = false;
       if (child.type === Tab) {
-        let selected = false;
-        if (
-          child.props.value === this.state.value ||
-          (!this.state.value && i === 0)
-        ) {
-          content = child.props.children;
+        if (this.isChildSelected(child.props.name, i)) {
+          content = <ContentDiv>{child.props.children}</ContentDiv>;
           selected = true;
         }
-        if (content) {
-          contentContainer = <TabContent>{content}</TabContent>;
-        }
-        return (
-          <Tab action={this.tabChanged} selected={selected} {...child.props} />
+        return React.cloneElement(child, {
+          selected,
+          action: this.tabChanged,
+          key: child.props.name
+        });
+      }
+      if (child.type === TabList) {
+        const options = React.Children.map(
+          child.props.children,
+          (secondChild, j) => {
+            if (this.isChildSelected(secondChild.props.name, i)) {
+              content = <ContentDiv>{secondChild.props.children}</ContentDiv>;
+              selected = true;
+            }
+            return { optionText: secondChild.props.name };
+          }
         );
+        return React.cloneElement(child, {
+          selected,
+          action: this.tabChanged,
+          options,
+          key: child.props.name
+        });
       }
       return null;
     });
@@ -92,16 +191,34 @@ class TabPanel extends React.Component {
     return (
       <div>
         {elements}
-        {contentContainer}
+        {content}
       </div>
     );
   }
 }
 
 TabPanel.propTypes = {
-  children: PropTypes.arrayOf(PropTypes.element).isRequired
+  children: (props, propName, component) => {
+    let badChild;
+    const isTabItem = child => {
+      const isTab = child.type.name === 'Tab' || child.type.name === 'TabList';
+      if (!isTab) {
+        badChild = child;
+      }
+      return isTab;
+    };
+    if (!props[propName].every(isTabItem)) {
+      return new Error(
+        `Tab Panel only accepts Tab or TabList as direct descendants. You gave a ${badChild
+          .type.name || badChild.type}`
+      );
+    }
+    return true;
+  }
 };
 
 TabPanel.Tab = Tab;
+TabPanel.TabList = TabList;
+TabPanel.TabItem = TabItem;
 
 export default TabPanel;
--- src/components/containers/tabPanel/TabPanel.md
+++ src/components/containers/tabPanel/TabPanel.md
@@ -2,21 +2,44 @@
 Testing text
 
 ```
-<TabPanel name="home">
-  <TabPanel.Tab name="Home" value="test">
+
+<TabPanel>
+  <TabPanel.Tab name="Home">
+    Lorem uipsyunkjaflklsadfkahs
+  </TabPanel.Tab>
+  <TabPanel.Tab name="Menu">
     <div>
-      Hi there
+      Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore 
     </div>
   </TabPanel.Tab>
-  <TabPanel.Tab name="menu" value="testing">
+  <TabPanel.Tab name="Display">
     <div>
-      Just another test
+      <p>
+        Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack
+      </p>
+      <ul>
+        <li>Item first</li>
+        <li>ItemSecond</li>
+      </ul>
     </div>
   </TabPanel.Tab>
-  <TabPanel.Tab name="Display" value="yetMoreTest">
-    <div>
-      Lipsom Epsum
-    </div>
-  </TabPanel.Tab>
+  <TabPanel.TabList name="many options">
+    <TabPanel.TabItem name="option1" value="TheFirst">
+      <div>
+        Hello there.
+      </div>
+    </TabPanel.TabItem>
+    <TabPanel.TabItem name="option2" value="TheSecond">
+      <div>
+        Lorem Ipsum
+      </div>
+    </TabPanel.TabItem>
+    <TabPanel.TabItem name="option3" value="TheThird">
+      <div>
+        Humbug
+      </div>
+    </TabPanel.TabItem>
+  </TabPanel.TabList>
+  <div></div>
 </TabPanel>
 ```

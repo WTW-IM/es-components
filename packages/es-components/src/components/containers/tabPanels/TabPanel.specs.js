9ec68beb3d0c896859ec1a20a6d73467049fdfb9
Nayshlok <borlandavid@gmail.com>
2018-04-23 08:50:02 -0600

    Fix tests for tab panel and snapshots

--- packages/es-components/src/components/containers/tabPanels/TabPanel.specs.js
+++ packages/es-components/src/components/containers/tabPanels/TabPanel.specs.js
@@ -14,52 +14,12 @@ describe('Tab panel component', () => {
       <TabPanel.Tab name="test2">
         <div>Test Content 2</div>
       </TabPanel.Tab>
-      <TabPanel.TabList name="testList">
-        <span optiontext="test3">
-          <div>Test Content 3</div>
-        </span>
-        <span optiontext="test4">
-          <div>Test Content 4</div>
-        </span>
-      </TabPanel.TabList>
-    </TabPanel>
-  );
-
-  const complexInstanceToRender = (
-    <TabPanel optionKeyFunc={x => x.replace(/\s/g, '')}>
-      <TabPanel.TabList name="testList">
-        <span optiontext="test3">
-          <div>Test Content 3</div>
-        </span>
-        <span optiontext="test4">
-          <div>Test Content 4</div>
-        </span>
-      </TabPanel.TabList>
-      <TabPanel.Tab name="test1">
-        <div>Test Content 1</div>
-      </TabPanel.Tab>
-      <TabPanel.Tab name="test2">
-        <div>Test Content 2</div>
-      </TabPanel.Tab>
     </TabPanel>
   );
 
   it('Will select first child on load', () => {
     const tabPanelInstance = mount(instanceToRender);
     expect(tabPanelInstance.state('value')).toBe('test1');
-  });
-
-  it('Will select first list item when rendering', () => {
-    const tabPanelInstance = mount(complexInstanceToRender);
-    expect(tabPanelInstance.state('value')).toBe('test3');
-  });
-
-  it('Properly selects a list', () => {
-    const tabPanelInstance = mount(instanceToRender);
-    tabPanelInstance.setState({ value: 'test3' });
-    expect(tabPanelInstance.find({ name: 'testList' }).props().selected).toBe(
-      true
-    );
   });
 
   it('Properly selects a tab', () => {

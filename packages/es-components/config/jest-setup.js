0265d6c9227cc099ed03fed17249a042250d19f7
Alex Abenoja <abenojaalex@gmail.com>
2018-06-18 09:48:56 -0600

    Clear requestAnimationFrame warnings
    * Move enzyme-adapter-react-16 from dependencies to devDependencies

--- packages/es-components/config/jest-setup.js
+++ packages/es-components/config/jest-setup.js
@@ -1,7 +1,4 @@
-import {noop} from 'lodash';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-global.requestAnimationFrame = noop;

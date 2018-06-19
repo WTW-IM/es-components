720d0917dc94bc8d7d1b9f3c102d4af70415d62a
Ken Adams <kenneth.adams5@extendhealth.com>
2018-06-19 12:55:54 -0600

    added drawer classes

--- packages/es-components/src/components/containers/drawer/Drawer.js
+++ packages/es-components/src/components/containers/drawer/Drawer.js
@@ -4,6 +4,7 @@ import styled, { ThemeProvider, withTheme } from 'styled-components';
 import { noop } from 'lodash';
 import uncontrollable from 'uncontrollable';
 import viaTheme from 'es-components-via-theme';
+import classNames from 'classNames';
 
 import DrawerPanel from './DrawerPanel';
 
@@ -89,7 +90,9 @@ export const Drawer = props => {
 
   return (
     <ThemeProvider theme={theme}>
-      <StyledDrawer className={className}>{getPanels()}</StyledDrawer>
+      <StyledDrawer className={classNames('es-drawer', className)}>
+        {getPanels()}
+      </StyledDrawer>
     </ThemeProvider>
   );
 };

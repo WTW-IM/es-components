f7a2f3557c45e8d7ff8758bc347af78c20d47d98
Ken Adams <kenneth.adams5@extendhealth.com>
2018-06-19 10:47:49 -0600

    added classNames package, updated Icon

--- packages/es-components/src/components/base/icons/Icon.js
+++ packages/es-components/src/components/base/icons/Icon.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import classNames from 'classNames';
 
 import { regular } from './icon-definitions';
 
@@ -34,7 +35,7 @@ function Icon({ className, name, size, ...other }) {
   return (
     <StyledIcon
       aria-hidden
-      className={className}
+      className={classNames('es-icon', className)}
       {...styledIconProps}
       {...other}
     />

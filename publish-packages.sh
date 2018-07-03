0fca5a7618818ec7a8ae9d1e89a5102de6d646c6
Ken Adams <kenneth.adams5@extendhealth.com>
2018-07-03 10:54:27 -0600

    added wtw theme to publish script

--- publish-packages.sh
+++ publish-packages.sh
@@ -8,4 +8,8 @@ cd ../es-components-via-theme
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 npm publish || true
 rm .npmrc
+cd ../es-components-wtw-theme
+echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
+npm publish || true
+rm .npmrc
 cd ../..

0e553dc6d1c7e658c4aeb03d481289314b4f014f
Alex Abenoja <abenojaalex@gmail.com>
2018-09-18 13:24:06 -0600

    Add tabChanged callback to TabPanel

--- packages/es-components/src/components/containers/tabPanels/TabPanel.specs.js
+++ packages/es-components/src/components/containers/tabPanels/TabPanel.specs.js
@@ -40,4 +40,35 @@ describe('Tab panel component', () => {
     tabButton.simulate('click');
     expect(tabPanelInstance.state('value')).toBe('test2');
   });
+
+  it('Clicking on tab invokes the callback', () => {
+    let selectedTab = 'test1';
+    const tabPanelInstance = mountWithTheme(instanceToRender);
+    tabPanelInstance.setProps({
+      tabChanged(name) {
+        selectedTab = name;
+      }
+    });
+
+    const tab = tabPanelInstance.find({ name: 'test2' });
+    const tabButton = tab.find('button');
+    tabButton.simulate('click');
+    expect(selectedTab).toBe('test2');
+  });
+
+  it('Clicking the selected tab does not invoke the callback unnecessarily', () => {
+    let timesCalled = 0;
+    const tabPanelInstance = mountWithTheme(instanceToRender);
+    tabPanelInstance.setProps({
+      tabChanged() {
+        timesCalled += 1;
+      }
+    });
+
+    const tab = tabPanelInstance.find({ name: 'test2' });
+    const tabButton = tab.find('button');
+    tabButton.simulate('click');
+    tabButton.simulate('click');
+    expect(timesCalled).toBe(1);
+  });
 });

a5e9d63e08d36c883baabba8e587abb3fcb05a8f
Steve Matney <steve.matney@extendhealth.com>
2018-09-19 15:06:36 -0600

    Shortening parameters in descriptionTitleProp.

--- packages/es-components/src/components/base/spinner/Spinner.js
+++ packages/es-components/src/components/base/spinner/Spinner.js
@@ -81,16 +81,11 @@ const Spinner = ({ title, description, ...props }) => {
   );
 };
 
-const descriptionTitleProp = (
-  props,
-  propName,
-  componentName,
-  ...otherParams
-) => {
+const descriptionTitleProp = (props, ...otherParams) => {
   if (!props.title && !props.description) {
     return new Error('You must provide a title, description, or both.');
   }
-  return PropTypes.string(props, propName, componentName, ...otherParams);
+  return PropTypes.string(props, ...otherParams);
 };
 
 Spinner.propTypes = {

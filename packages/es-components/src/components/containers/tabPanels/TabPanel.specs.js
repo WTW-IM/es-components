7e43c6a532757273271c351f9a6e80fb47357db0
Jake Rios <jacob.rios2@extendhealth.com>
2018-10-30 14:24:55 -0600

    Update: Change TabPanel to use react-testing-library

--- packages/es-components/src/components/containers/tabPanels/TabPanel.specs.js
+++ packages/es-components/src/components/containers/tabPanels/TabPanel.specs.js
@@ -1,99 +1,65 @@
 /* eslint-env jest */
 
 import React from 'react';
-import { mountWithTheme, renderWithTheme } from 'styled-enzyme';
+import { render, cleanup } from 'react-testing-library';
+import { ThemeProvider } from 'styled-components';
+import viaTheme from 'es-components-via-theme';
 
 import TabPanel from './TabPanel';
 
-describe('Tab panel component', () => {
-  const instanceToRender = (
-    <TabPanel optionKeyFunc={x => x.replace(/\s/g, '')}>
-      <TabPanel.Tab name="test1">
-        <div>Test Content 1</div>
-      </TabPanel.Tab>
-      <TabPanel.Tab name="test2">
-        <div>Test Content 2</div>
-      </TabPanel.Tab>
-    </TabPanel>
+beforeEach(cleanup);
+
+it('displays first tab when rendered', () => {
+  const { getByText, queryByText } = render(
+    <ThemeProvider theme={viaTheme}>
+      <TabPanel>
+        <TabPanel.Tab name="tab 1">Tab number 1</TabPanel.Tab>
+        <TabPanel.Tab name="tab 2">Tab number 2</TabPanel.Tab>
+      </TabPanel>
+    </ThemeProvider>
   );
 
-  const instanceWithAnnouncer = (
-    <TabPanel optionKeyFunc={x => x.replace(/\s/g, '')}>
-      <TabPanel.Tab
-        name="test1"
-        simpleName="I'm Simple Name!"
-        announcerText="Why am I not just part of simpleName?"
-      >
-        <div>Test Content 1</div>
-      </TabPanel.Tab>
-      <TabPanel.Tab name="test2">
-        <div>Test Content 2</div>
-      </TabPanel.Tab>
-    </TabPanel>
+  expect(getByText('Tab number 1')).toBeVisible();
+  expect(queryByText('Tab number 2')).toBeNull();
+});
+
+it('displays content for tab when corresponding button is clicked', () => {
+  const { getByText, queryByText } = render(
+    <ThemeProvider theme={viaTheme}>
+      <TabPanel>
+        <TabPanel.Tab name="tab 1">Tab number 1</TabPanel.Tab>
+        <TabPanel.Tab name="tab 2">Tab number 2</TabPanel.Tab>
+      </TabPanel>
+    </ThemeProvider>
   );
 
-  it('renders as expected', () => {
-    const tree = renderWithTheme(instanceToRender).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+  getByText('tab 2').click();
 
-  it('renders as expected with aria announcer text', () => {
-    const tree = renderWithTheme(instanceWithAnnouncer).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+  expect(queryByText('Tab number 1')).toBeNull();
+  expect(getByText('Tab number 2')).toBeVisible();
+});
 
-  it('Will select first child on load', () => {
-    const tabPanelInstance = mountWithTheme(instanceToRender);
-    expect(tabPanelInstance.state('value')).toBe('test1');
-  });
+it('invokes the passed tabChanged prop only when the tab changes', () => {
+  const tabChanged = jest.fn();
+  const { getByText } = render(
+    <ThemeProvider theme={viaTheme}>
+      <TabPanel tabChanged={tabChanged}>
+        <TabPanel.Tab name="tab 1">Tab number 1</TabPanel.Tab>
+        <TabPanel.Tab name="tab 2">Tab number 2</TabPanel.Tab>
+      </TabPanel>
+    </ThemeProvider>
+  );
 
-  it('Properly selects a tab', () => {
-    const tabPanelInstance = mountWithTheme(instanceToRender);
-    tabPanelInstance.setState({ value: 'test2' });
-    expect(
-      tabPanelInstance
-        .find({ name: 'test2' })
-        .first()
-        .props().selected
-    ).toBe(true);
-  });
+  const secondTab = getByText('tab 2');
 
-  it('Clicks on tab select them', () => {
-    const tabPanelInstance = mountWithTheme(instanceToRender);
-    const tab = tabPanelInstance.find({ name: 'test2' });
-    const tabButton = tab.find('button');
-    tabButton.simulate('click');
-    expect(tabPanelInstance.state('value')).toBe('test2');
-  });
+  secondTab.click();
+  expect(tabChanged).toHaveBeenCalled();
 
-  it('Clicking on tab invokes the callback', () => {
-    let selectedTab = 'test1';
-    const tabPanelInstance = mountWithTheme(instanceToRender);
-    tabPanelInstance.setProps({
-      tabChanged(name) {
-        selectedTab = name;
-      }
-    });
+  tabChanged.mockClear();
 
-    const tab = tabPanelInstance.find({ name: 'test2' });
-    const tabButton = tab.find('button');
-    tabButton.simulate('click');
-    expect(selectedTab).toBe('test2');
-  });
+  secondTab.click();
+  expect(tabChanged).not.toHaveBeenCalled();
 
-  it('Clicking the selected tab does not invoke the callback unnecessarily', () => {
-    let timesCalled = 0;
-    const tabPanelInstance = mountWithTheme(instanceToRender);
-    tabPanelInstance.setProps({
-      tabChanged() {
-        timesCalled += 1;
-      }
-    });
-
-    const tab = tabPanelInstance.find({ name: 'test2' });
-    const tabButton = tab.find('button');
-    tabButton.simulate('click');
-    tabButton.simulate('click');
-    expect(timesCalled).toBe(1);
-  });
+  getByText('tab 1').click();
+  expect(tabChanged).toHaveBeenCalled();
 });

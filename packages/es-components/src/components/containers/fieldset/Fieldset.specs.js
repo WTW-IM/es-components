23edd8c24e94fa96702bea5a5c0c2d86a6634ece
Jake Rios <jacob.rios2@extendhealth.com>
2018-10-30 11:23:21 -0600

    Update: Change FieldSet tests to react-testing-library

--- packages/es-components/src/components/containers/fieldset/Fieldset.specs.js
+++ packages/es-components/src/components/containers/fieldset/Fieldset.specs.js
@@ -1,35 +1,20 @@
 /* eslint-env jest */
 
 import React from 'react';
-import { mountWithTheme, renderWithTheme } from 'styled-enzyme';
+import { render } from 'react-testing-library';
+import { ThemeProvider } from 'styled-components';
+import viaTheme from 'es-components-via-theme';
 
 import Fieldset from './Fieldset';
 
-describe('Fieldset component', () => {
-  it('adds additional classes to legend element when aditionalLegendClasses prop is supplied', () => {
-    const instance = mountWithTheme(
-      <Fieldset legendContent="I am legend" legendClasses="legendary" />
-    );
-    expect(instance.find('legend').hasClass('legendary')).toBe(true);
-  });
-
-  it('renders as expected', () => {
-    const tree = renderWithTheme(
-      <Fieldset>
-        <div>Fieldset child</div>
-      </Fieldset>
-    );
-
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('renders as expected with legendText', () => {
-    const tree = renderWithTheme(
+it('renders legend when legendContent is provided', () => {
+  const { queryByText } = render(
+    <ThemeProvider theme={viaTheme}>
       <Fieldset legendContent="I am legend">
         <div>Fieldset child</div>
       </Fieldset>
-    );
+    </ThemeProvider>
+  );
 
-    expect(tree).toMatchSnapshot();
-  });
+  expect(queryByText('I am legend')).not.toBeNull();
 });

d6a17fd5717120ad8a2645eb6e23ea5c6c98cd44
Jake Rios <jacob.rios2@extendhealth.com>
2018-11-04 14:14:21 -0700

    Update: Change Modal to use react-testing-library

--- packages/es-components/src/components/containers/modal/Modal.specs.js
+++ packages/es-components/src/components/containers/modal/Modal.specs.js
@@ -1,23 +1,85 @@
 /* eslint-env jest */
 
 import React from 'react';
-import { mountWithTheme } from 'styled-enzyme';
-import Modal from './Modal';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import { ThemeProvider } from 'styled-components';
+import viaTheme from 'es-components-via-theme';
 
-describe('modal component', () => {
-  let instance;
+import Modal from './Modal';
+import ModalHeader from './ModalHeader';
+import ModalBody from './ModalBody';
+import ModalFooter from './ModalFooter';
+
+jest.mock('../../util/generateAlphaName', () => () => 'abcdef');
+
+beforeEach(cleanup);
+
+function renderModal(modalProps) {
+  return render(
+    <ThemeProvider theme={viaTheme}>
+      <Modal show {...modalProps}>
+        <ModalHeader>Header</ModalHeader>
+        <ModalBody>Body</ModalBody>
+        <ModalFooter>Footer</ModalFooter>
+      </Modal>
+    </ThemeProvider>
+  );
+}
+
+it('invokes functions when triggered', () => {
+  const onEnter = jest.fn();
   const onHide = jest.fn();
 
-  beforeEach(() => {
-    instance = mountWithTheme(
-      <Modal show onHide={onHide} animation={false}>
-        <div className="body">Modal body text.</div>
+  const { getByText } = renderModal({ onEnter, onHide });
+
+  expect(onEnter).toHaveBeenCalled();
+
+  getByText('Header')
+    .parentElement.querySelector('button')
+    .click();
+  expect(onHide).toHaveBeenCalled();
+});
+
+it('renders different modal sections', () => {
+  const onEnter = jest.fn();
+  const onHide = jest.fn();
+
+  const { getByText } = renderModal({ onEnter, onHide });
+
+  expect(onEnter).toHaveBeenCalled();
+
+  expect(getByText('Header')).toMatchSnapshot();
+  expect(getByText('Body')).toMatchSnapshot();
+  expect(getByText('Footer')).toMatchSnapshot();
+});
+
+it('hides dismiss button when hideCloseButton is true', () => {
+  const { getByText } = render(
+    <ThemeProvider theme={viaTheme}>
+      <Modal show>
+        <ModalHeader hideCloseButton>Header</ModalHeader>
+        <ModalBody>Body</ModalBody>
+        <ModalFooter>Footer</ModalFooter>
       </Modal>
-    );
+    </ThemeProvider>
+  );
+  expect(getByText('Header').parentElement.querySelector('button')).toBeNull();
+});
+
+describe('when ESC is pressed', () => {
+  it('invokes onHide by default', () => {
+    const onHide = jest.fn();
+    const { container } = renderModal({ onEnter: jest.fn(), onHide });
+
+    fireEvent.keyDown(container, { keyCode: 27 });
+    expect(onHide).toHaveBeenCalled();
   });
 
-  it('mounts as expected', () => {
-    expect(instance.find(Modal).length).toBe(1);
-    expect(instance.find(Modal).prop('show')).toBe(true);
+  it('does not invoke onHide when escapeExits is false', () => {
+    const onHide = jest.fn();
+    const { container } = renderModal({ onHide, escapeExits: false });
+
+    fireEvent.keyDown(container, { keyCode: 27 });
+    expect(onHide).not.toHaveBeenCalled();
   });
 });

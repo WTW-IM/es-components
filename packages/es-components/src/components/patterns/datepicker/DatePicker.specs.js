a68ac5c09c4650a124a07c9eeb0e47c1a99a2f99
Jake Rios <jacob.rios2@extendhealth.com>
2018-11-07 14:56:25 -0700

    Update: Change Datepicker to use react-testing-library

--- packages/es-components/src/components/patterns/datepicker/DatePicker.specs.js
+++ packages/es-components/src/components/patterns/datepicker/DatePicker.specs.js
@@ -1,44 +1,23 @@
 /* eslint-env jest */
 
 import React from 'react';
-import { mountWithTheme } from 'styled-enzyme';
+import { render } from 'react-testing-library';
+import { ThemeProvider } from 'styled-components';
+import viaTheme from 'es-components-via-theme';
+import moment from 'moment';
 
 import DatePicker from './DatePicker';
 
-describe('DatePicker component', () => {
-  let instance;
-  let input;
-  const handleOnChange = jest.fn();
-  const handleOnChangeRaw = jest.fn();
-  const mockOnChangeRawEvent = { target: { value: 'some date' } };
-  const mockOnChangeEvent = { target: { value: '10/12/2017' } };
-  /* eslint-disable jsx-a11y/use-onblur-not-onchange */
-  beforeEach(() => {
-    instance = mountWithTheme(
+it('renders DatePicker when textbox is focused', () => {
+  const { getByLabelText, queryByText } = render(
+    <ThemeProvider theme={viaTheme}>
       <DatePicker
-        labelText="Test Date"
-        onChange={handleOnChange}
-        onChangeRaw={handleOnChangeRaw}
+        labelText="Test date"
+        onChange={jest.fn()}
+        selectedDate={moment(new Date(2018, 10, 7))}
       />
-    );
-
-    input = instance.find('input');
-    handleOnChange.mockClear();
-    handleOnChangeRaw.mockClear();
-  });
-  /* eslint-enable */
-  it('executes the handleOnChange function when valid date is entered', () => {
-    input.simulate('change', mockOnChangeEvent);
-    expect(handleOnChange).toBeCalled();
-  });
-
-  it('does not execute handleOnChange unless date is valid', () => {
-    input.simulate('change', mockOnChangeRawEvent);
-    expect(handleOnChange).not.toBeCalled();
-  });
-
-  it('executes the handleOnChangeRaw function when any input is entered', () => {
-    input.simulate('change', mockOnChangeRawEvent);
-    expect(handleOnChangeRaw).toBeCalled();
-  });
+    </ThemeProvider>
+  );
+  getByLabelText('Test date').focus();
+  expect(queryByText('November 2018')).not.toBeNull();
 });

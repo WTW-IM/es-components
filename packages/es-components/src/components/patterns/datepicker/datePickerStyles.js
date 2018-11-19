3c1636bd55e565ef837341aeabde0fedd726ba35
Alex Abenoja <abenojaalex@gmail.com>
2018-11-19 15:27:18 -0700

    Fix: replace injectGlobal with createGlobalStyle

--- packages/es-components/src/components/patterns/datepicker/datePickerStyles.js
+++ packages/es-components/src/components/patterns/datepicker/datePickerStyles.js
@@ -1,5 +1,9 @@
-export default function datepickerStyles(colors, dpColors) {
-  return `
+import React, { useContext } from 'react';
+import { createGlobalStyle, ThemeContext } from 'styled-components';
+
+export function DatepickerStyles() {
+  const { colors, datepickerColors: dpColors } = useContext(ThemeContext);
+  const Styles = createGlobalStyle`
   .react-datepicker-popper[data-placement^="bottom"] .react-datepicker__triangle,
   .react-datepicker-popper[data-placement^="top"] .react-datepicker__triangle,
   .react-datepicker__year-read-view--down-arrow,
@@ -566,4 +570,5 @@ export default function datepickerStyles(colors, dpColors) {
     cursor: default;
   }
 `;
+  return <Styles />;
 }

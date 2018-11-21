ffd663df58acc5353e33dacae4649c091a479888
Alex Abenoja <abenojaalex@gmail.com>
2018-11-21 09:28:09 -0700

    Upgrade: use latest babel-preset-wtw-im
    Latest preset (2.3.1) uses the recently introduced
    babelrc configs as defaults. .babelrc.js has been
    updated accordingly

--- packages/es-components/.babelrc.js
+++ packages/es-components/.babelrc.js
@@ -11,7 +11,6 @@ if (process.env['NODE_ENV'] === 'test') {
 module.exports = {
   presets: [['wtw-im', wtwConfig]],
   plugins: [
-    '@babel/plugin-transform-runtime',
     '@babel/plugin-proposal-export-default-from'
   ]
 };

30da755696c9dd9ab04a3c7b9d512489e4cfacdb
Alex Abenoja <abenojaalex@gmail.com>
2018-11-21 12:43:11 -0700

    Fix: use ExampleWrapper without compilation

--- packages/es-components/src/styleguide/ExampleWrapper.js
+++ packages/es-components/src/styleguide/ExampleWrapper.js
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
 
-import React from 'react';
+import { createElement } from 'react';
 import { ThemeProvider } from 'styled-components';
 import viaTheme from 'es-components-via-theme';
 
 export default function ExampleWrapper({ children }) {
-  return <ThemeProvider theme={viaTheme}>{children}</ThemeProvider>;
+  return createElement(ThemeProvider, { theme: viaTheme }, children);
 }

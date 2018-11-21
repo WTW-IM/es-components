ba468c934267d4a6857c543de45a421c60447c82
Alex Abenoja <abenojaalex@gmail.com>
2018-11-21 12:43:58 -0700

    Fix: remove object destructure on propTypes import

--- packages/es-components/src/components/patterns/incrementer/Incrementer.js
+++ packages/es-components/src/components/patterns/incrementer/Incrementer.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { PropTypes } from 'prop-types';
+import PropTypes from 'prop-types';
 import { noop, isNumber } from 'lodash';
 import styled, { withTheme } from 'styled-components';
 

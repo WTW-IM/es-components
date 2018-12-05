ac9b9162f102e073fb9699d024e810122c1443b8
Alex Abenoja <abenojaalex@gmail.com>
2018-12-05 14:30:18 -0700

    Update: wrap all examples in strict mode

--- packages/es-components/src/styleguide/ExampleWrapper.js
+++ packages/es-components/src/styleguide/ExampleWrapper.js
@@ -1,9 +1,14 @@
 /* eslint-disable react/prop-types */
 
-import { createElement } from 'react';
+import React, { createElement } from 'react';
 import { ThemeProvider } from 'styled-components';
 import viaTheme from 'es-components-via-theme';
 
-export default function ExampleWrapper({ children }) {
-  return createElement(ThemeProvider, { theme: viaTheme }, children);
+export default function ExampleWrapper(props) {
+  const WrapperType = React.StrictMode;
+  return createElement(
+    WrapperType,
+    {},
+    createElement(ThemeProvider, { theme: viaTheme }, props.children)
+  );
 }

1a68ac385293b6ecc2c632e9db2e51cda269d003
Scott Bartlett <scott.bartlett@extendhealth.com>
2019-01-31 09:13:36 -0700

    Update: adds software blue color

--- packages/es-components-via-theme/index.js
+++ packages/es-components-via-theme/index.js
@@ -100,7 +100,8 @@ const theme = {
     greenLighter: greenLighter,
     violetLighter: violetLighter,
     blueLighter: blueLighter,
-    violet: violet
+    violet: violet,
+    softwareBlue: darkBlue
   },
   brandColors: {
     vbBlue: vbBlue,

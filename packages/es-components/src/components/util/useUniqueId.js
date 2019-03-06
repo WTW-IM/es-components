ce37fd96331ccbad0d57b4185e9d6ed6a9284c75
Steve Matney <steve.matney@extendhealth.com>
2019-03-06 10:00:06 -0700

    Update: use useUniqueId in AnswerButton

--- packages/es-components/src/components/util/useUniqueId.js
+++ packages/es-components/src/components/util/useUniqueId.js
@@ -1,7 +1,7 @@
 import { useRef } from 'react';
 import generateAlphaName from './generateAlphaName';
 
-export default function useUniqueId() {
-  const idRef = useRef(generateAlphaName());
+export default function useUniqueId(providedId) {
+  const idRef = useRef(providedId || generateAlphaName());
   return idRef.current;
 }

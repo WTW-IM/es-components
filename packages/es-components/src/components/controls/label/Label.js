ee1b325fd1936bde5ad8bad26572e2166419abfe
Jake Rios <jacob.rios2@extendhealth.com>
2019-03-15 10:44:11 -0600

    Fix: set spacing for labels properly

--- packages/es-components/src/components/controls/label/Label.js
+++ packages/es-components/src/components/controls/label/Label.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Label = styled.label`
   color: inherit;
   cursor: pointer;
+  display: flex;
   font-size: ${props => props.theme.sizes.baseFontSize};
   font-weight: 700;
   margin-bottom: 5px;

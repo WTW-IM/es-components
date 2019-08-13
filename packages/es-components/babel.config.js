9295a4fe51ff8015adf032a656ef42e51c153a5a
Darrken <ken.james.adams@gmail.com>
2019-08-13 08:22:08 -0600

    Build: switched back to babel-preset-wtw-im

--- packages/es-components/babel.config.js
+++ packages/es-components/babel.config.js
@@ -3,31 +3,24 @@ module.exports = function(api) {
 
   const presets = [
     [
-      '@babel/preset-env',
+      'wtw-im',
       {
-        useBuiltIns: 'usage',
-        corejs: 3,
-        modules: modules,
-        debug: false
+        extractFormatMessage: false,
+        transformFormatMessage: false,
+        env: {
+          useBuiltIns: 'usage',
+          corejs: 3,
+          modules: modules
+        }
       }
-    ],
-    '@babel/preset-react'
+    ]
   ];
 
   const plugins = [
     ['@babel/plugin-transform-runtime', { corejs: 3 }],
-    'babel-plugin-lodash',
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-class-properties',
-    ['transform-react-remove-prop-types', { mode: 'wrap' }],
-    [
-      'babel-plugin-styled-components',
-      {
-        ssr: false,
-        displayName: false,
-        fileName: false
-      }
-    ]
+    ['transform-react-remove-prop-types', { mode: 'wrap' }]
   ];
 
   return {

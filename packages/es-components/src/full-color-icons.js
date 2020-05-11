21bc657f954192ee2eb19db5d9e8ac92d696ca78
Steve Matney <steve.matney@extendhealth.com>
2020-05-11 15:58:23 -0600

    Docs: adding filtering to the icons

--- packages/es-components/src/full-color-icons.js
+++ packages/es-components/src/full-color-icons.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import styled, { ThemeProvider } from 'styled-components';
 import viaTheme from 'es-components-via-theme';
 import ReactDOM from 'react-dom';
+import { debounce } from 'lodash';
 import Spinner from './components/base/spinner/Spinner';
 import FullColorIcon from './components/base/icons/FullColorIcon';
 
@@ -36,21 +37,69 @@ const SpinnerContainer = styled.div`
   width: 100px;
 `;
 
-function DisplayFCIcon({ name }) {
+const HighlightSpan = styled.span`
+  background-color: rgba(113, 194, 255, 0.65);
+`;
+
+function DisplayName({ name, highlight }) {
+  const highlightStart = name.indexOf(highlight);
+  if (!highlight.length || highlightStart < 0) return name;
+  const values = useMemo(() => {
+    const highlightEnd = highlightStart + highlight.length;
+    const pre = highlightStart > 0 ? name.substring(0, highlightStart) : '';
+    const post =
+      highlightEnd >= name.length ? '' : name.substring(highlightEnd);
+    return { pre, post };
+  }, [name, highlight]);
   return (
-    <IconContainer>
-      <FullColorIcon name={name} size={100} css={{ margin: '0 auto' }} />
-      <div css={{ marginTop: 10 }}>{name}</div>
+    <div css={{ marginTop: 10 }}>
+      {values.pre}
+      <HighlightSpan>{highlight}</HighlightSpan>
+      {values.post}
+    </div>
+  );
+}
+
+DisplayName.propTypes = {
+  name: PropTypes.string.isRequired,
+  highlight: PropTypes.string.isRequired
+};
+
+function DisplayFCIcon({ name, highlight, ...props }) {
+  return (
+    <IconContainer {...props}>
+      <FullColorIcon
+        name={name}
+        highlight={highlight}
+        size={100}
+        css={{ margin: '0 auto' }}
+      />
+      <DisplayName name={name} highlight={highlight} />
     </IconContainer>
   );
 }
 
 DisplayFCIcon.propTypes = {
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  highlight: PropTypes.string.isRequired
 };
 
 function FullColorIconsApp() {
   const [icons, setIcons] = useState([]);
+  const [iconFilter, setIconFilter] = useState('');
+
+  const iconDisplayValue = useCallback(
+    name => (name.includes(iconFilter) ? 'inherit' : 'none'),
+    [iconFilter]
+  );
+
+  const debouncedSetFilter = useCallback(
+    debounce(value => setIconFilter(value), 150)
+  );
+  const filterVisible = useCallback(({ target: { value } }) =>
+    debouncedSetFilter(value)
+  );
+
   useEffect(() => {
     const getIcons = async () => {
       const iconsResponse = await fetch(iconsListUrl);
@@ -71,14 +120,35 @@ function FullColorIconsApp() {
     };
     getIcons();
   }, []);
+
   return (
     <OuterContainer>
       {icons.length ? (
-        <IconsContainer>
-          {icons.map(name => (
-            <DisplayFCIcon name={name} key={name} />
-          ))}
-        </IconsContainer>
+        <>
+          <div css={{ marginBottom: 40 }}>
+            <label htmlFor="filter">
+              <h2 css={{ display: 'inline' }}>Filter Icons:</h2>
+              <input
+                css={{ marginLeft: 10 }}
+                id="filter"
+                onChange={filterVisible}
+                type="text"
+              />
+            </label>
+          </div>
+          <IconsContainer>
+            {icons.map(name => (
+              <DisplayFCIcon
+                css={{
+                  display: iconDisplayValue(name)
+                }}
+                name={name}
+                highlight={iconFilter}
+                key={name}
+              />
+            ))}
+          </IconsContainer>
+        </>
       ) : (
         <SpinnerContainer>
           <Spinner />

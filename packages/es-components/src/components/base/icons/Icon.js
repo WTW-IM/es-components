80e34286363c648e9aa5457b3c0ea1efba2c64a9
Darrken <ken.james.adams@gmail.com>
2021-02-01 16:01:35 -0700

    Update: removed icon definitions file, now uses classes from CDN

--- packages/es-components/src/components/base/icons/Icon.js
+++ packages/es-components/src/components/base/icons/Icon.js
@@ -2,48 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import { regular } from './icon-definitions';
-
 const StyledIcon = styled.i`
   display: inline-block;
-  font-family: bds-func-icons !important;
-  font-size: ${props => props.fontSize};
-  font-style: normal;
-  font-weight: inherit;
-  font-variant: normal;
-  line-height: 1;
-  speak: none;
+  font-size: ${props => props.size};
   text-decoration: none;
-  text-transform: none;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   vertical-align: text-bottom;
-
-  &:before {
-    content: "\\e${props => props.content}";
-  }
 `;
 
-function Icon({ name, size, ...other }) {
-  const iconName = `im-icon-${name}`;
-
-  const styledIconProps = {
-    content: regular[iconName],
-    fontSize: size !== undefined ? `${size}px` : 'inherit'
-  };
-
-  return <StyledIcon aria-hidden {...styledIconProps} {...other} />;
+function Icon({ name, size, className, ...other }) {
+  return (
+    <StyledIcon
+      className={`bds-icon bds-${name}${className ? ` ${className}` : ''}`}
+      size={size ? `${size}px` : 'inherit'}
+      aria-hidden
+      {...other}
+    />
+  );
 }
 
 Icon.propTypes = {
   /** Name of the icon to display */
   name: PropTypes.string.isRequired,
   /** Specify icon size in pixels */
-  size: PropTypes.number
+  size: PropTypes.number,
+  /** Additional classes to include */
+  className: PropTypes.string
 };
 
 Icon.defaultProps = {
-  size: undefined
+  size: undefined,
+  className: undefined
 };
 
 export default Icon;

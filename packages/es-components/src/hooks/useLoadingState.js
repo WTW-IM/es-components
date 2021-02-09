d714d375e4e4b825a4fdbb76fc284af87e62c1de
Stewart Anderson <stewart.anderson3@extendhealth.com>
2021-02-08 17:58:14 -0700

    Update: (WE-10763) min loading time

--- packages/es-components/src/hooks/useLoadingState.js
+++ packages/es-components/src/hooks/useLoadingState.js
@@ -1,10 +1,17 @@
 import { useMountedOnlyState } from './useMountedOnlyState';
 
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+const minLoadingTime = 500;
+
 export function useLoadingState() {
   const [isLoading, setIsLoading] = useMountedOnlyState(false);
   async function start(theOperation) {
     setIsLoading(true);
     try {
+      if (minLoadingTime) {
+        await wait(minLoadingTime);
+      }
+
       return await theOperation;
     } finally {
       setIsLoading(false);

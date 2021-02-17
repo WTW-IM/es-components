32faa9cf7d5e8a4fc5a1c65e736dbd7ae5ca920b
Steve Matney <steve.matney@extendhealth.com>
2021-02-17 12:31:14 -0700

    Update: utilized waiting prop in withLoadingStateWhileRunning and added tests
    Co-authored-by: dabraham5 <desean.abraham@extendhealth.com>

--- packages/es-components/src/components/controls/buttons/features/withLoadingStateWhileRunning.js
+++ packages/es-components/src/components/controls/buttons/features/withLoadingStateWhileRunning.js
@@ -1,37 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import { useLoadingState } from '../../../../hooks/useLoadingState';
 
 export const withLoadingStateWhileRunning = ButtonComponent => {
-  const StyledButton = styled(ButtonComponent)`
-    ${({ displayAsRunning }) =>
-      displayAsRunning &&
-      `
-      :hover{
-        color: white;
-      }
-      :focus{
-        box-shadow: 0 1px 1px grey, 0 0 0 0.2rem grey;
-        color: lightgrey;
-        background-color: grey;
-        border-color: grey;
-      }
-      color: lightgrey;
-      background-color: grey;
-      cursor: not-allowed;
-      border-color: grey;
-    `}
-  `;
-
   const ButtonWithLoadingState = React.forwardRef(
     (
       {
         showWhileRunning: runningContent,
-        isWaiting,
         children,
         onClick,
-        styleType,
+        waiting,
         ...otherProps
       },
       ref
@@ -39,34 +17,31 @@ export const withLoadingStateWhileRunning = ButtonComponent => {
       const [isRunning, showRunningWhile] = useLoadingState();
       const runOperation = (...params) =>
         runningContent
-          ? !isRunning && !isWaiting && showRunningWhile(onClick(...params))
+          ? !isRunning && showRunningWhile(onClick(...params))
           : onClick(...params);
 
       return (
-        <StyledButton
+        <ButtonComponent
           {...otherProps}
-          styleType={runningContent && isRunning ? undefined : styleType}
-          displayAsRunning={isWaiting || isRunning}
+          waiting={waiting || isRunning}
           onClick={runOperation}
           ref={ref}
         >
           {runningContent && isRunning ? runningContent : children}
-        </StyledButton>
+        </ButtonComponent>
       );
     }
   );
 
   ButtonWithLoadingState.propTypes = {
+    ...ButtonComponent.propTypes,
     showWhileRunning: PropTypes.any,
-    isWaiting: PropTypes.bool,
     children: PropTypes.node.isRequired,
-    onClick: PropTypes.func,
-    styleType: PropTypes.string
+    onClick: PropTypes.func
   };
 
   ButtonWithLoadingState.defaultProps = {
-    styleType: 'default',
-    isWaiting: false,
+    ...ButtonComponent.defaultProps,
     showWhileRunning: undefined,
     onClick: undefined
   };

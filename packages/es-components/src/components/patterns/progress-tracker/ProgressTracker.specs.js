b283886049b0bf23f92cac2eac587f10d3c98471
Emma Scanlon <emma.scanlon@extendhealth.com>
2022-07-27 10:19:19 -0600

    Update: fix test by mocking useUniqueId

--- packages/es-components/src/components/patterns/progress-tracker/ProgressTracker.specs.js
+++ packages/es-components/src/components/patterns/progress-tracker/ProgressTracker.specs.js
@@ -4,12 +4,26 @@ import React from 'react';
 import { cleanup } from '@testing-library/react';
 
 import { renderWithTheme } from '../../util/test-utils';
+import useUniqueId from '../../util/useUniqueId';
 import ProgressTracker from './ProgressTracker';
+
+jest.mock('../../util/useUniqueId');
 
 function getInstance(props) {
   return renderWithTheme(<ProgressTracker {...props} />);
 }
 
+beforeEach(async () => {
+  await useUniqueId.mockImplementationOnce(() => '1');
+  await useUniqueId.mockImplementationOnce(() => '2');
+  await useUniqueId.mockImplementationOnce(() => '3');
+  await useUniqueId.mockImplementationOnce(() => '1');
+  await useUniqueId.mockImplementationOnce(() => '2');
+  await useUniqueId.mockImplementationOnce(() => '3');
+  await useUniqueId.mockImplementationOnce(() => '1');
+  await useUniqueId.mockImplementationOnce(() => '2');
+  await useUniqueId.mockImplementationOnce(() => '3');
+});
 it('builds a ProgressTracker', () => {
   const stepsToTest = [
     [

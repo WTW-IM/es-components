42c6797577bf09d2523b707ebbeee9a907ac4be6
Steve Matney <steve.matney@extendhealth.com>
2023-01-13 17:54:16 -0700

    Build: fixing DateInput tests

--- packages/es-components/src/components/controls/dropdown/Dropdown.js
+++ packages/es-components/src/components/controls/dropdown/Dropdown.js
@@ -4,7 +4,7 @@ import InputBase, { useValidationStyleProps } from '../textbox/InputBase';
 
 const DropdownInput = styled(InputBase)`
   min-width: 100px;
-  box-shadow: 0 0 0 0 $({ borderColor }) => borderColor};
+  box-shadow: 0 0 0 0 ${({ borderColor }) => borderColor};
 `;
 
 export default function Dropdown(props) {

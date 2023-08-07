9e83d53386fcbbffb40a5403a3ed2fb22e21f74e
Steve Matney <steve.matney@extendhealth.com>
2023-08-07 17:36:00 -0600

    Fix: truly allowing for optional useMountedOnlyState initial value

--- packages/es-components/src/hooks/useMountedOnlyState.ts
+++ packages/es-components/src/hooks/useMountedOnlyState.ts
@@ -3,13 +3,16 @@ import { useRef, useEffect, useState, useCallback } from 'react';
 export type OriginalState<T> = T | (() => T);
 export type SetStateFunction<T> = React.Dispatch<React.SetStateAction<T>>;
 
-export function useMountedOnlyState<T = undefined>(
-  origState: OriginalState<T | undefined>
-): [T | undefined, SetStateFunction<T | undefined>];
+export function useMountedOnlyState<T>(): [
+  T | undefined,
+  SetStateFunction<T | undefined>
+];
 export function useMountedOnlyState<T>(
   origState: OriginalState<T>
 ): [T, SetStateFunction<T>];
-export function useMountedOnlyState<T>(origState?: OriginalState<T>) {
+export function useMountedOnlyState<T>(
+  origState?: OriginalState<T>
+): [T | undefined, SetStateFunction<T | undefined>] {
   const isMountedRef = useRef(true);
   const [state, setState] = useState(origState);
   const setStateOrNoOp = useCallback<SetStateFunction<T | undefined>>(
@@ -28,5 +31,5 @@ export function useMountedOnlyState<T>(origState?: OriginalState<T>) {
     []
   );
 
-  return [state, setStateOrNoOp] as const;
+  return [state, setStateOrNoOp];
 }

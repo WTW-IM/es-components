d4ad207645d565840ef257de4781c6f817d5fe72
Mario Lozano <Mario.Lozano@extendhealth.com>
2024-07-02 14:03:51 -0500

    Fix: update OutlineButton to include forwardedAs prop

--- packages/es-components/src/components/controls/buttons/OutlineButton.tsx
+++ packages/es-components/src/components/controls/buttons/OutlineButton.tsx
@@ -25,13 +25,6 @@ const StyledButton = styled(Button)<{
   border: 2px solid ${props => props.colors.textColor};
   color: ${props => props.colors.textColor};
   overflow: hidden;
-  min-width: 100px;
-  padding-bottom: ${props => props.buttonSize.paddingBottom};
-  padding-left: ${props => props.buttonSize.paddingSides};
-  padding-right: ${props => props.buttonSize.paddingSides};
-  padding-top: ${props => props.buttonSize.paddingTop};
-  text-align: center;
-  text-decoration: none;
 
   &:focus {
     box-shadow: 0 0 0 0.2rem ${props => props.colors.focusBoxShadowColor};
@@ -89,12 +82,17 @@ const StyledButton = styled(Button)<{
   }
 `;
 
-const OutlineButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+type OutlineButtonProps = ButtonProps & {
+  as?: string | React.ComponentType<unknown>;
+};
+
+const OutlineButton = React.forwardRef<HTMLButtonElement, OutlineButtonProps>(
   function OutlineButton(props, ref) {
     const {
       children,
       styleType = 'default',
       size = 'default',
+      as,
       ...other
     } = props;
     const theme = useTheme();
@@ -134,6 +132,7 @@ const OutlineButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
 
     return (
       <StyledButton
+        forwardedAs={as}
         ref={ref}
         buttonSize={buttonSize}
         colors={buttonColors}
